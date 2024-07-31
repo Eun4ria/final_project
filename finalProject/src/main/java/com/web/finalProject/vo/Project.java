904e068127a5ee69dbc0ecd8b3506ee338e506cf
human-18 <human-18@DESKTOP-10VCR83>
2024-07-31 19:11:48 +0900

    24.07.31

---
+++ finalProject/src/main/java/com/web/finalProject/vo/Project.java
@@ -0,0 +1,60 @@
+package com.web.finalProject.vo;
+
+public class Project {
+	private String project_name;
+	private String project_id;
+	private String etc;
+	private String start_date;
+	private String end_date;
+	private String create_date;
+	private boolean pstatus;
+	private String company_id;
+	public String getProject_name() {
+		return project_name;
+	}
+	public void setProject_name(String project_name) {
+		this.project_name = project_name;
+	}
+	public String getProject_id() {
+		return project_id;
+	}
+	public void setProject_id(String project_id) {
+		this.project_id = project_id;
+	}
+	public String getEtc() {
+		return etc;
+	}
+	public void setEtc(String etc) {
+		this.etc = etc;
+	}
+	public String getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(String start_date) {
+		this.start_date = start_date;
+	}
+	public String getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(String end_date) {
+		this.end_date = end_date;
+	}
+	public String getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(String create_date) {
+		this.create_date = create_date;
+	}
+	public boolean isPstatus() {
+		return pstatus;
+	}
+	public void setPstatus(boolean pstatus) {
+		this.pstatus = pstatus;
+	}
+	public String getCompany_id() {
+		return company_id;
+	}
+	public void setCompany_id(String company_id) {
+		this.company_id = company_id;
+	}
+}
